692f6d95da7b1572190c1974edb305b762a94c52
Cesar Cardoso de Oliveira <96243515+ccoliveira91@users.noreply.github.com>
2023-02-15 18:05:23 -0300

    commit-1

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
+
+export function DefaultLayout() {
+    return (
+        <div>
+            <LayoutContainer>
+                <Header />
+                <Outlet /> {/*Tudo que for especifico de uma página vai ser demonstrado em Outlet */}
+            </LayoutContainer>
+        </div>
+    )
+}
